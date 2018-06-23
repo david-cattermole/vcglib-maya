4bb47bef8e374d5d5975661e1439bd07e8605120
david-cattermole <cattermole91@gmail.com>
2018-06-23 18:36:57 +0100

    Clean up the test commands. Don't run Maya tests by default. Ensure vcgNodes is in the Maya plugin path.

--- test/test.sh
+++ test/test.sh
@@ -1,50 +1,49 @@
 #!/usr/bin/env bash
 
 ROOT=`pwd`;
+OUTPUT=${ROOT}/output;
+BUILD_DIR=${ROOT}/../cmake-build-debug;
+DATA_DIR=${ROOT}/../data;
 
 
 # Mesh Stats
-../cmake-build-debug/meshStats ../data/gargoyle_align_base.obj ${ROOT}/gargoyle_stats_out.obj;
-../cmake-build-debug/meshStats ../data/gargoyle_align_p1.obj ${ROOT}/gargoyle_stats_out.obj;
-../cmake-build-debug/meshStats ../data/gargoyle_align_p2.obj ${ROOT}/gargoyle_stats_out.obj;
- ../cmake-build-debug/meshStats ../data/gargoyle_align_p3.obj ${ROOT}/gargoyle_stats_out.obj;
- ../cmake-build-debug/meshStats ../data/gargoyle_align_p4.obj ${ROOT}/gargoyle_stats_out.obj;
- ../cmake-build-debug/meshStats ../data/fertility_texCoords.obj ${ROOT}/fertility_stats_out.obj;
- ../cmake-build-debug/meshStats ../data/sphere.obj ${ROOT}/sphere_stats_out.obj;
- ../cmake-build-debug/meshStats ../data/trisphere.obj ${ROOT}/sphere_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/gargoyle_align_base.obj ${OUTPUT}/gargoyle_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/gargoyle_align_p1.obj ${OUTPUT}/gargoyle_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/gargoyle_align_p2.obj ${OUTPUT}/gargoyle_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/gargoyle_align_p3.obj ${OUTPUT}/gargoyle_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/gargoyle_align_p4.obj ${OUTPUT}/gargoyle_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/fertility_texCoords.obj ${OUTPUT}/fertility_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/sphere.obj ${OUTPUT}/sphere_stats_out.obj;
+${BUILD_DIR}/meshStats ${DATA_DIR}/trisphere.obj ${OUTPUT}/sphere_stats_out.obj;
 
 
-## Very fast tests
-#../cmake-build-debug/decimator ../data/quad.ply ${ROOT}/test_quad_100.obj 100;
-#../cmake-build-debug/decimator ../data/quad.ply ${ROOT}/test_quad_050.obj 50;
+# Very fast tests
+${BUILD_DIR}/decimator ${DATA_DIR}/quad.ply ${OUTPUT}/test_quad_100.obj 100;
+${BUILD_DIR}/decimator ${DATA_DIR}/quad.ply ${OUTPUT}/test_quad_050.obj 50;
 
 
-# Maya Tests
- mayapy runVcgNodesTests.py
+# Fast tests
+${BUILD_DIR}/decimator ${DATA_DIR}/fertility.ply ${OUTPUT}/test_fertility_100.obj 100 --verbose;
+${BUILD_DIR}/decimator ${DATA_DIR}/fertility_texCoords.obj ${OUTPUT}/test_fertility_100_uvs.obj 100  --verbose;
+${BUILD_DIR}/decimator ${DATA_DIR}/fertility.ply ${OUTPUT}/test_fertility_010.obj 10 --verbose;
+${BUILD_DIR}/decimator ${DATA_DIR}/fertility_texCoords.obj ${OUTPUT}/test_fertility_010_uvs.obj 10 --verbose;
 
 
-## Fast tests
-#../cmake-build-debug/decimator ../data/fertility.ply ${ROOT}/test_fertility_100.obj 100 --verbose;
-#../cmake-build-debug/decimator ../data/fertility_texCoords.obj ${ROOT}/test_fertility_100_uvs.obj 100  --verbose;
-#../cmake-build-debug/decimator ../data/fertility.ply ${ROOT}/test_fertility_010.obj 10 --verbose;
-#../cmake-build-debug/decimator ../data/fertility_texCoords.obj ${ROOT}/test_fertility_010_uvs.obj 10 --verbose;
+# # Maya Tests
+# env MAYA_PLUG_IN_PATH=${MAYA_PLUG_IN_PATH}:${BUILD_DIR}/  mayapy ${ROOT}/runVcgNodesTests.py
 
 
-## Alignment tests
-#../cmake-build-debug/autoAlign ../data/gargoyle_align_base.obj ../data/gargoyle_align_p1.obj  ${ROOT}/gargoyle_align_p1_out.obj;
-#../cmake-build-debug/autoAlign ../data/gargoyle_align_base.obj ../data/gargoyle_align_p2.obj  ${ROOT}/gargoyle_align_p2_out.obj;
-#../cmake-build-debug/autoAlign ../data/gargoyle_align_base.obj ../data/gargoyle_align_p3.obj  ${ROOT}/gargoyle_align_p3_out.obj;
-#../cmake-build-debug/autoAlign ../data/gargoyle_align_base.obj ../data/gargoyle_align_p4.obj  ${ROOT}/gargoyle_align_p4_out.obj;
+# # Alignment tests
+# ${BUILD_DIR}/autoAlign ${DATA_DIR}/gargoyle_align_base.obj ${DATA_DIR}/gargoyle_align_p1.obj  ${OUTPUT}/gargoyle_align_p1_out.obj;
+# ${BUILD_DIR}/autoAlign ${DATA_DIR}/gargoyle_align_base.obj ${DATA_DIR}/gargoyle_align_p2.obj  ${OUTPUT}/gargoyle_align_p2_out.obj;
+# ${BUILD_DIR}/autoAlign ${DATA_DIR}/gargoyle_align_base.obj ${DATA_DIR}/gargoyle_align_p3.obj  ${OUTPUT}/gargoyle_align_p3_out.obj;
+# ${BUILD_DIR}/autoAlign ${DATA_DIR}/gargoyle_align_base.obj ${DATA_DIR}/gargoyle_align_p4.obj  ${OUTPUT}/gargoyle_align_p4_out.obj;
 
 
-## Medium tests
-#../cmake-build-debug/decimator ../data/ganesha.ply ${ROOT}/test_ganesha_100.obj 100;
-#../cmake-build-debug/decimator ../data/ganesha.ply ${ROOT}/test_ganesha_010.obj 10;
+# Medium tests
+${BUILD_DIR}/decimator ${DATA_DIR}/ganesha.ply ${OUTPUT}/test_ganesha_100.obj 100;
+${BUILD_DIR}/decimator ${DATA_DIR}/ganesha.ply ${OUTPUT}/test_ganesha_010.obj 10;
 
 
-## Slow tests
-#../cmake-build-debug/decimator ../data/nano_out_pc100.obj ${ROOT}/test_slow_001.obj 1  --verbose;
-
-
-## Very Slow Tests
-#../cmake-build-debug/decimator ../data/fxphd_all5Mil_fromMayaUVs.obj ${ROOT}/test_verySlow_001.obj 1 --verbose;
+# Slow tests
+${BUILD_DIR}/decimator ${DATA_DIR}/nano_out_pc100.obj ${OUTPUT}/test_slow_001.obj 1  --verbose;
